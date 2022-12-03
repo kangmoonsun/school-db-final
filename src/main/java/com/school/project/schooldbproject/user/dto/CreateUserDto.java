6241f7f7f4ddcbc8fc86fee0e9804880b2c546ee
KGESH <seonghyeon.jee@gmail.com>
2022-12-03 18:15:15 +0900

    Refactor: NotNull 데코레이터 추가

--- src/main/java/com/school/project/schooldbproject/user/dto/CreateUserDto.java
+++ src/main/java/com/school/project/schooldbproject/user/dto/CreateUserDto.java
@@ -5,11 +5,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Builder
 public class CreateUserDto {
+    @NotNull
     private String name;
+
+    @NotNull
     private String address;
 
     public User toEntity() {
